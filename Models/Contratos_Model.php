cc91fde1acc592c8a4079de8bd061ebadcb02dff
Alex-notfound <curras.alex@hotmail.com>
2018-12-21 20:53:06 +0100

    Update Contratos_Model.php

--- Models/Contratos_Model.php
+++ Models/Contratos_Model.php
@@ -13,53 +13,42 @@ class Contratos_Model {
 //Funcion encargada de extraer los datos del SHOWALL
     function showAll() {
         $showAll = "SELECT * from contratos";
-        $resultado = $this->mysqli->query($showAll);
-        return $resultado;
+        return $this->mysqli->query($showAll);
     }
 
 //Funcion encargada de extraer los datos del SHOWCURRENT
-    function showCurrent($email) {
+    function showCurrent($cod) {
         $showCurrent = "SELECT * from contratos WHERE `cod`='$cod'";
-        $resultadoCurrent = $this->mysqli->query($showCurrent);
-        return $resultadoCurrent;
+        return $this->mysqli->query($showCurrent);
     }
 
 //Metodo ADD
 //Inserta en la tabla  de la bd  los valores
 // de los atributos del objeto. Comprueba si la clave/s esta vacia y si 
 //existe ya en la tabla
-    function ADD() {
-        
-    }
-
-//funcion de destrucción del objeto: se ejecuta automaticamente
-//al finalizar el script
-    function __destruct() {
-        
+    function ADD($cod, $centro, $tipo, $cifEmpresa, $documento, $periodoInicio, $periodoFin, $importe) {
+        $add = "INSERT INTO `contratos` (`cod`, `centro`, `tipo`, `cifEmpresa`, `documento`, `periodoinicio`, `periodofin`, `importe`) 
+        VALUES ('$cod', '$centro', '$tipo', '$cifEmpresa', '$documento', '$periodoInicio', '$periodoFin', '$importe')";
+        return $this->mysqli->query($add);
     }
 
 //funcion SEARCH: hace una búsqueda en la tabla con
 //los datos proporcionados. Si van vacios devuelve todos
     function SEARCH() {
-        
     }
 
 //funcion DELETE : comprueba que la tupla a borrar existe y una vez
 // verificado la borra
-    function DELETE() {
-        
-    }
-
-// funcion RellenaDatos: recupera todos los atributos de una tupla a partir de su clave
-    function RellenaDatos() {
-        
+    function DELETE($cod) {
+        $delete = "DELETE FROM `contratos` WHERE `cod`='$cod'";
+        return $this->mysqli->query($delete);
     }
 
 // funcion Edit: realizar el update de una tupla despues de comprobar que existe
-    function EDIT() {
-        
+    function EDIT($cod, $centro, $tipo, $cifEmpresa, $documento, $periodoInicio, $periodoFin, $importe) {
+        $edit = "UPDATE `contratos` SET `centro`='$centro',`tipo`='$tipo',`cifEmpresa`='$cifEmpresa',`documento`='$documento',"
+                . "`periodoinicio`='$periodoInicio',`periodofin`='$periodoFin',`importe`='$importe' WHERE `cod`='$cod'";
     }
-
 }
 
 //fin de clase
