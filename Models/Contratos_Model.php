34c372467b2ae4b074b73ddb73fab9f1967b43fd
Alex-notfound <curras.alex@hotmail.com>
2018-12-21 21:12:48 +0100

    Update Contratos_Model.php
    Falta SEARCH

--- Models/Contratos_Model.php
+++ Models/Contratos_Model.php
@@ -2,10 +2,26 @@
 
 class Contratos_Model {
 
-    var $mysqli;
+    var $cod;
+    var $centro;
+    var $tipo;
+    var $cifEmpresa;
+    var $documento;
+    var $periodoInicio;
+    var $periodoFin;
+    var $importe;
 
 //Constructor de la clase
-    function __construct() {
+    function __construct($cod, $centro, $tipo, $cifEmpresa, $documento, $periodoInicio, $periodoFin, $importe) {
+        $this->cod = $cod;
+        $this->centro = $centro;
+        $this->tipo = $tipo;
+        $this->cifEmpresa = $cifEmpresa;
+        $this->documento = $documento;
+        $this->periodoInicio = $periodoInicio;
+        $this->periodoFin = $periodoFin;
+        $this->importe = $importe;
+
         include_once '../Models/Access_DB.php';
         $this->mysqli = ConnectDB();
     }
@@ -13,42 +29,60 @@ class Contratos_Model {
 //Funcion encargada de extraer los datos del SHOWALL
     function showAll() {
         $showAll = "SELECT * from contratos";
-        return $this->mysqli->query($showAll);
+        if (!($resultado = $this->mysqli->query($showAll))) {
+            return true;
+        } else {
+            return $resultado;
+        }
     }
 
 //Funcion encargada de extraer los datos del SHOWCURRENT
-    function showCurrent($cod) {
-        $showCurrent = "SELECT * from contratos WHERE `cod`='$cod'";
-        return $this->mysqli->query($showCurrent);
+    function showCurrent() {
+        $showCurrent = "SELECT * from contratos WHERE `cod`='$this->cod'";
+        if (!($resultado = $this->mysqli->query($showCurrent))) {
+            return true;
+        } else {
+            $result = $resultado->fetch_array();
+            return $result;
+        }
     }
 
 //Metodo ADD
 //Inserta en la tabla  de la bd  los valores
 // de los atributos del objeto. Comprueba si la clave/s esta vacia y si 
 //existe ya en la tabla
-    function ADD($cod, $centro, $tipo, $cifEmpresa, $documento, $periodoInicio, $periodoFin, $importe) {
+    function ADD() {
         $add = "INSERT INTO `contratos` (`cod`, `centro`, `tipo`, `cifEmpresa`, `documento`, `periodoinicio`, `periodofin`, `importe`) 
-        VALUES ('$cod', '$centro', '$tipo', '$cifEmpresa', '$documento', '$periodoInicio', '$periodoFin', '$importe')";
-        return $this->mysqli->query($add);
+        VALUES ('$this->cod', '$this->centro', '$this->tipo', '$this->cifEmpresa', '$this->documento', '$this->periodoInicio', '$this->periodoFin', '$this->importe')";
+        if (!$this->mysqli->query($add)) {
+            return 'Error en la inserción';
+        }
+        return 'Participación añadida con éxito'; //operacion de insertado correcta
     }
 
 //funcion SEARCH: hace una búsqueda en la tabla con
 //los datos proporcionados. Si van vacios devuelve todos
     function SEARCH() {
+        
     }
 
 //funcion DELETE : comprueba que la tupla a borrar existe y una vez
 // verificado la borra
-    function DELETE($cod) {
-        $delete = "DELETE FROM `contratos` WHERE `cod`='$cod'";
-        return $this->mysqli->query($delete);
+    function DELETE() {
+        $delete = "DELETE FROM `contratos` WHERE `cod`='$this->cod'";
+        if (!$this->mysqli->query($delete)) {
+            return 'Error en la inserción';
+        } else {
+            return 'Eliminación realizada con éxito';
+        }
     }
 
 // funcion Edit: realizar el update de una tupla despues de comprobar que existe
-    function EDIT($cod, $centro, $tipo, $cifEmpresa, $documento, $periodoInicio, $periodoFin, $importe) {
-        $edit = "UPDATE `contratos` SET `centro`='$centro',`tipo`='$tipo',`cifEmpresa`='$cifEmpresa',`documento`='$documento',"
-                . "`periodoinicio`='$periodoInicio',`periodofin`='$periodoFin',`importe`='$importe' WHERE `cod`='$cod'";
+    function EDIT() {
+        $edit = "UPDATE `contratos` SET `centro`='$this->centro',`tipo`='$this->tipo',`cifEmpresa`='$this->cifEmpresa',`documento`='$this->documento',"
+                . "`periodoinicio`='$this->periodoInicio',`periodofin`='$this->periodoFin',`importe`='$this->importe' WHERE `cod`='$this->cod'";
     }
+
 }
 
 //fin de clase
