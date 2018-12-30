ef650d5bf865770dab6bd4949b59bb50789946bb
Alex-notfound <curras.alex@hotmail.com>
2018-12-30 22:44:12 +0100

    Finalizada la clase Contratos_Model.php

--- Models/Contratos_Model.php
+++ Models/Contratos_Model.php
@@ -2,25 +2,10 @@
 
 class Contratos_Model {
 
-    var $cod;
-    var $centro;
-    var $tipo;
-    var $cifEmpresa;
-    var $documento;
-    var $periodoInicio;
-    var $periodoFin;
-    var $importe;
+    var $mysqli;
 
 //Constructor de la clase
-    function __construct($cod, $centro, $tipo, $cifEmpresa, $documento, $periodoInicio, $periodoFin, $importe) {
-        $this->cod = $cod;
-        $this->centro = $centro;
-        $this->tipo = $tipo;
-        $this->cifEmpresa = $cifEmpresa;
-        $this->documento = $documento;
-        $this->periodoInicio = $periodoInicio;
-        $this->periodoFin = $periodoFin;
-        $this->importe = $importe;
+    function __construct() {
 
         include_once '../Models/Access_DB.php';
         $this->mysqli = ConnectDB();
@@ -30,6 +15,7 @@ class Contratos_Model {
     function showAll() {
         $showAll = "SELECT * from contratos";
         if (!($resultado = $this->mysqli->query($showAll))) {
+            echo "NO HAY TUPLAS";
             return true;
         } else {
             return $resultado;
@@ -38,7 +24,7 @@ class Contratos_Model {
 
 //Funcion encargada de extraer los datos del SHOWCURRENT
     function showCurrent() {
-        $showCurrent = "SELECT * from contratos WHERE `cod`='$this->cod'";
+        $showCurrent = "SELECT * from contratos WHERE `cod`='cod'";
         if (!($resultado = $this->mysqli->query($showCurrent))) {
             return true;
         } else {
@@ -53,7 +39,7 @@ class Contratos_Model {
 //existe ya en la tabla
     function ADD() {
         $add = "INSERT INTO `contratos` (`cod`, `centro`, `tipo`, `cifEmpresa`, `documento`, `periodoinicio`, `periodofin`, `importe`) 
-        VALUES ('$this->cod', '$this->centro', '$this->tipo', '$this->cifEmpresa', '$this->documento', '$this->periodoInicio', '$this->periodoFin', '$this->importe')";
+        VALUES ('cod', 'centro', 'tipo', 'cifEmpresa', 'documento', 'periodoInicio', 'periodoFin', 'importe')";
         if (!$this->mysqli->query($add)) {
             return 'Error en la inserción';
         }
@@ -63,13 +49,21 @@ class Contratos_Model {
 //funcion SEARCH: hace una búsqueda en la tabla con
 //los datos proporcionados. Si van vacios devuelve todos
     function SEARCH() {
-        
+        $search = "SELECT * FROM contratos WHERE `cod` LIKE '%" . cod . "%' AND `centro` LIKE '%" . centro . "%' AND `tipo` LIKE '%" . tipo . "%' AND
+             `cifEmpresa` LIKE '%" . cifEmpresa . "%' AND `documento` LIKE '%" . documento . "%' AND `periodoInicio` LIKE '%" . periodoInicio . "%' AND
+                  `periodoFin` LIKE '%" . periodoFin . "%' AND `importe` LIKE '%" . importe . "%'";
+        if (!($resultado = $this->mysqli->query($search))) {
+            return true;
+        } else {
+            $result = $resultado->fetch_array();
+            return $result;
+        }
     }
 
 //funcion DELETE : comprueba que la tupla a borrar existe y una vez
 // verificado la borra
     function DELETE() {
-        $delete = "DELETE FROM `contratos` WHERE `cod`='$this->cod'";
+        $delete = "DELETE FROM `contratos` WHERE `cod`='cod'";
         if (!$this->mysqli->query($delete)) {
             return 'Error en la inserción';
         } else {
@@ -79,8 +73,8 @@ class Contratos_Model {
 
 // funcion Edit: realizar el update de una tupla despues de comprobar que existe
     function EDIT() {
-        $edit = "UPDATE `contratos` SET `centro`='$this->centro',`tipo`='$this->tipo',`cifEmpresa`='$this->cifEmpresa',`documento`='$this->documento',"
-                . "`periodoinicio`='$this->periodoInicio',`periodofin`='$this->periodoFin',`importe`='$this->importe' WHERE `cod`='$this->cod'";
+        $edit = "UPDATE `contratos` SET `centro`='centro',`tipo`='tipo',`cifEmpresa`='cifEmpresa',`documento`='documento',"
+                . "`periodoinicio`='periodoInicio',`periodofin`='periodoFin',`importe`='importe' WHERE `cod`='cod'";
     }
 
 }
