4fcc40a495459b2bdac911780c63d40cdf60e259
Wildrou <victorakawild@gmail.com>
2019-01-16 15:37:15 +0100

    Instalable funciona!

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-chmod 777 /var/www/html/ET4_Bravo/Models/BD_IU2.sql
-mysql -h 127.0.0.1 -u root -p IU2018</var/www/html/ET4_Bravo/Models/BD_IU2.sql
+chmod 777 ./ET4_Bravo/Models/BD_IU2.sql
+mysql  -uroot -p <./ET4_Bravo/Models/BD_IU2.sql
