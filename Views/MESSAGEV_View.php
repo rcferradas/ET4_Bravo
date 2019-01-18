0d2f7730d93494f4005b2c97b6a85ba84dd3a6ce
Miarpe <miguelariasperez@gmail.com>
2019-01-18 13:39:30 +0100

    Botón tras mensaje de visita

--- Views/MESSAGEV_View.php
+++ Views/MESSAGEV_View.php
@@ -35,7 +35,7 @@ class MESSAGEV {
          <form class="form-inline my-2 my-lg-0" name='formulario' action=<?php echo $this->volver ?> method="">
                           <input type="hidden" name=codcontrato value=<?php echo $this->codigocon ?>>
                            <button name="action" value="SHOWALL" type="submit" class="btn btn-outline-primary">
-                             
+                           <i class="fa fa-arrow-left"></i></button>&nbsp  
          </form>
         <?php
         
